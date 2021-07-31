c25c00378527c6819747312a7cc7a92ebddeeca1
crystal66666 <77876633+crystal66666@users.noreply.github.com>
2021-07-31 00:02:21 -0700

    add ordering to backend
    1. orders
    2. addorder

--- backend/userModels.js
+++ backend/userModels.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
 
-module.exports =mongoose.model('project-register', {
-    email: { type: String, unique:true, default:'', required:true },
-    password: { type:String, default:'', required:true },
-    username:{type:String, unique: true,default:'', required:true },
-    isadmin:{type:Boolean,default: false, required:true }
+module.exports = mongoose.model('project-register', {
+  email: { type: String, unique: true, default: '', required: true },
+  password: { type: String, default: '', required: true },
+  username: { type: String, unique: true, default: '', required: true },
+  isadmin: { type: Boolean, default: false, required: true },
+  nextid: { type: Number, default: 0 },
+  orders: [{
+    orderid: { type: String, required: true },
+    products: [{
+      name: { type: String, required: true },
+      size: { type: String, default: '' },
+      ice: { type: String, default: '' },
+      sweetness: { type: String, default: '' },
+      extras: { type: [String] },
+      quantity: { type: Number, required: true }
+    }]
+  }]
 });
--- backend/userroutes.js
+++ backend/userroutes.js
@@ -1,98 +1,142 @@
-const Sample =require('./userModels');
-const filter = {password:0, __v:0 }
+const Sample = require('./userModels');
+const filter = { password: 0, __v: 0 }
 const jwt = require('jsonwebtoken');
-const md5= require('blueimp-md5')
+const md5 = require('blueimp-md5')
 
-const auth = async(req,res) =>{
-    const raw = String(req.headers.authorization).split(' ').pop();
-    const {id} = jwt.verify(raw,process.env.JWT_KEY)
-    req.user = await Sample.findById(id)
-}
-
-function checkFormat(data,type){
-    var regPassword=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{6,12}$/;
-    var regExEmail=/([\w\.]+)@([\w\.]+)\.(\w+)/;
-    if(type==0){
-        if(!String(data).match(regPassword)){
-            return false;
-        }else {
-            return true;
-        }
-    }else{
-        if(!String(data).match(regExEmail)){
-            return false;
-        }else {
-            return true;
-        }
+const auth = async (req, res) => {
+  const raw = String(req.headers.authorization).split(' ').pop();
+  if (raw) {
+    try {
+      const { id } = jwt.verify(raw, process.env.JWT_KEY);
+      req.user = await Sample.findById(id);
     }
+    catch(err) {
+      // todo
+      req.user = null;
+    }
+  }
+};
+
+function checkFormat(data, type) {
+  var regPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{6,12}$/;
+  var regExEmail = /([\w\.]+)@([\w\.]+)\.(\w+)/;
+  if (type == 0) {
+    if (!String(data).match(regPassword)) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    if (!String(data).match(regExEmail)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
-module.exports =(app) =>{
-    app.post('/signup', function (req,res){
-        let {email,username,password}=req.body;
-        if(checkFormat(req.body.email,1) ) {
-            if (checkFormat(req.body.password,0)) {
-                Sample.findOne({email}, function (err, data) {
-                    if (data) {
-                        res.send({code:1, msg: 'Email already registered!'})
-                    } else {
-                        Sample.findOne({username}, function (err, data) {
-                            if(data){
-                                res.send({code:1, msg: 'username already registered!'})
-                            }else {
-                                new Sample({email, username, password: md5(password)}).save(function (error, user) {
-                                    if (error) res.send({code: 1, msg: 'Server disconnected'})
-                                    const data = {email, username, _id: email._id}
-                                    res.cookie('emailid', email._id, {maxAge: 1000 * 60 * 60 * 24})
-                                    res.send({code: 0, data})
-                                })
-                            }
-                        })
-                    };
-                });
-            } else {
-                res.send({code:1, msg: 'password too simple!'})
-            }
-        }else{
-            res.send({code:1, msg: 'email error!'})
-        }
-    });
-
-   app.post("/signin", async function (req,res){
-        const {email,password }=req.body;
-        Sample.findOne({email, password:md5(password)},filter,function (err,data){
-            if(data){
-                const token = jwt.sign({
-                    isAuth: true,
-                    username: data.username,
-                    id:String(data._id)
-                },
-                    process.env.JWT_KEY,
-                    {
-                        expiresIn: "1h"
-                    }
-                );
-                const alldata ={_id:data._id,email: data.email,isadmin: data.isadmin, token}
-                res.send({
-                    code:0,
-                    data:alldata,
+module.exports = (app) => {
+  app.post('/signup', function (req, res) {
+    let { email, username, password } = req.body;
+    console.log(JSON.stringify(req.body));
+    if (checkFormat(req.body.email, 1)) {
+      if (checkFormat(req.body.password, 0)) {
+        Sample.findOne({ email }, function (err, data) {
+          if (data) {
+            res.send({ code: 1, msg: 'Email already registered!' })
+          } else {
+            Sample.findOne({ username }, function (err, data) {
+              if (data) {
+                res.send({ code: 1, msg: 'username already registered!' })
+              } else {
+                new Sample({ email, username, password: md5(password) }).save(function (error, user) {
+                  if (error) res.send({ code: 1, msg: 'Server disconnected' })
+                  const data = { email, username, _id: email._id }
+                  res.cookie('emailid', email._id, { maxAge: 1000 * 60 * 60 * 24 })
+                  res.send({ code: 0, data })
                 })
-            }else{
-                res.send({code:1 , msg: "Invalid email or password"});
-            }
-        })
-    })
-
-    app.get('/',(req,res) =>{
-
-        Sample.find(function (err,samples){
-            if(err)
-                res.send(err);
-            console.log('samples',samples);
-            res.status(201);
-            res.json(samples);
+              }
+            })
+          };
         });
+      } else {
+        res.send({ code: 1, msg: 'password too simple!' })
+      }
+    } else {
+      res.send({ code: 1, msg: 'email error!' })
+    }
+  });
+
+  app.post("/signin", async function (req, res) {
+    const { email, password } = req.body;
+    Sample.findOne({ email, password: md5(password) }, filter, function (err, data) {
+      if (data) {
+        const token = jwt.sign({
+          isAuth: true,
+          username: data.username,
+          id: String(data._id)
+        },
+          process.env.JWT_KEY,
+          {
+            expiresIn: "1h"
+          }
+        );
+        const alldata = { _id: data._id, email: data.email, isadmin: data.isadmin, token }
+        res.send({
+          code: 0,
+          data: alldata,
+        })
+      } else {
+        res.send({ code: 1, msg: "Invalid email or password" });
+      }
+    })
+  });
+
+  app.post("/addorder", async function (req, res) {
+    const { products } = req.body;
+    // todo: validate products
+    auth(req, res)
+      .then(() => {
+        if (req.user) {
+          const nextid = req.user.nextid;
+          const newOrder = {
+            orderid: nextid,
+            products: products
+          }
+          Sample.updateOne({ _id: req.user._id }, {nextid: nextid + 1, $push: {orders: newOrder}}, function(err, data) {
+            if (data.nModified === 1) {
+              res.send({ code: 0, data: { orderid: nextid }});
+            } else {
+              res.send({ code: 1, msg: "Could not submit order"});
+            }
+          });
+        } else {
+          res.send({ code: 1, msg: "Invalid login" });
+        }
+      });
+  });
+
+  app.get("/orders", async function (req, res) {
+    auth(req, res)
+      .then(() => {
+        if (req.user) {
+          res.send({ code: 0, data: req.user.orders });
+        } else {
+          res.send({ code: 1, msg: "Invalid login" });
+        }
+      });
+  });
+
+  app.get('/', (req, res) => {
+
+    Sample.find(function (err, samples) {
+      if (err)
+        res.send(err);
+      console.log('samples', samples);
+      res.status(201);
+      res.json(samples);
     });
+  });
 
 
 }
