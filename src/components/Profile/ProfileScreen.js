075fc89d013c6b5d4b53aa0da2b4be44da20d949
crystal66666 <77876633+crystal66666@users.noreply.github.com>
2021-08-01 00:34:11 -0700

    add profile screen

--- src/components/Profile/ProfileScreen.js
+++ src/components/Profile/ProfileScreen.js
@@ -1,22 +1,88 @@
-import React, {Component} from 'react';
-import {reqProfile} from "../../api";
+import React, { Component } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { reqProfile, reqUpdateProfile } from "../../api";
+import "./Profile.css"
+
 class ProfileScreen extends Component {
-    async componentDidMount() {
-       const {data} =await reqProfile()
-       console.log(data)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      profile: {
+        firstname: "",
+        lastname: "",
+        phone: "",
+        address: "",
+      }
+    };
+  }
+  async componentDidMount() {
+    const { data } = await reqProfile();
+    this.setState({
+      username: data.data.username,
+      email: data.data.email,
+      profile: data.data.profile
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                Personal Information<hr/>
-                Personal Information<hr/>
-                Personal Information<hr/>
-                Personal Information<hr/>
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div id="profile-container">
+        <Formik
+          enableReinitialize={true}
+          initialValues={{
+            username: this.state.username,
+            email: this.state.email,
+            firstname: this.state.profile.firstname,
+            lastname: this.state.profile.lastname,
+            phone: this.state.profile.phone,
+            address: this.state.profile.address
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            const data = {
+              profile: {
+                firstname: values.firstname,
+                lastname: values.lastname,
+                phone: values.phone,
+                address: values.address
+              }
+            }
+            reqUpdateProfile(data)
+              .then(response => {
+                setSubmitting(false);
+              });
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <label htmlFor="username">User name</label>
+              <Field type="text" name="username" disabled />
+              <ErrorMessage name="username" component="div" />
+              <label htmlFor="email">Email</label>
+              <Field type="email" name="email" disabled />
+              <ErrorMessage name="email" component="div" />
+              <label htmlFor="firstname">First name</label>
+              <Field type="text" name="firstname" />
+              <ErrorMessage name="firstname" component="div" />
+              <label htmlFor="lastname">Last name</label>
+              <Field type="text" name="lastname" />
+              <ErrorMessage name="lastname" component="div" />
+              <label htmlFor="phone">Phone number</label>
+              <Field type="text" name="phone" />
+              <ErrorMessage name="phone" component="div" />
+              <label htmlFor="address">Address</label>
+              <Field type="text" name="address" />
+              <ErrorMessage name="address" component="div" />
+              <button type="submit" disabled={isSubmitting}>
+                Update
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
 }
 
 export default ProfileScreen;
