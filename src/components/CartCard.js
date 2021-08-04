11a6cc384ffdbf92c5d18a500e17f411b5c2c3ab
Yitong Guo <guoyitong2018@gmail.com>
2021-08-04 10:31:00 -0700

    fix cart img loading bug

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import productInfo from "../model/productInfo.json"
 
 const CartCard = ({reading}) => {
-    console.log(reading);
+    console.log("reading:" + reading);
+    var products = productInfo.products;
+    var imgFile = "";
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].title === reading.product) {
+            imgFile = products[i].imgFileName;
+            break;
+        }
+    }
     return (
         <div className="card" style={{width: "540px"}}>
             <div className="card-body">
                 <h4 className="card-title">{reading.product}</h4>
-                <img className="orderImg" src={"../../img/"+reading.product.replace(/\s/g, '')+".jpg"} />
+                <img className="orderImg" src={"../../img/" +imgFile} />
                 <div style={{display: "inline-block"}}>
                 <p className="card-text">
                     Size: {reading.size}
--- src/components/Order.js
+++ src/components/Order.js
@@ -42,24 +42,6 @@ class Order extends React.Component {
                         </div>
                     </Tab>
                 </Tabs>
-                {/* <div  className = "orderNav">
-                    <Nav variant="dark" defaultActiveKey="/order/#milkTeaOrder" as="ul">
-                    <Nav.Item as="li">
-                        <Nav.Link href="/order/#milkTeaOrder"><h4 className="linkText">Milk Tea</h4></Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item as="li">
-                        <Nav.Link href="/order/#fruitTeaOrder"><h4 className="linkText">Fruit Tea</h4></Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item as="li">
-                        <Nav.Link href="/order/#specialOrder"><h4 className="linkText">Specialty Drinks</h4></Nav.Link>
-                    </Nav.Item>
-                    </Nav>
-                </div> */}
-                
-                {/* <div className = "teaContent">
-                    <OrderTea products = {this.state.productInfo}/>
-                </div> */}
-
             </div>
             
         )
