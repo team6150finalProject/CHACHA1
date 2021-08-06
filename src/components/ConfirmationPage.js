d569ddfe74548436d842026c45e1c16f92d5830e
Snihil <sun.yiche@northeastern.edu>
2021-08-07 00:06:55 +0800

    confirmationPage Connected

--- src/components/ConfirmationPage.js
+++ src/components/ConfirmationPage.js
@@ -1,46 +1,75 @@
 import React from 'react';
+import {connect} from "react-redux";
+import {fetchData} from "../redux/actions";
+import OrderCard from "./User/OrderCard";
 
 class ConfirmationPage extends React.Component {
 
     constructor(props) {
-
         super(props);
+        this.state = {
+            order: {
+                timemillis: 0,
+                price: 0,
+                location: "",
+                products: [{
+                    name: "",
+                    size: "",
+                    price: "",
+                    ice: "",
+                    sweetness: "",
+                    extras: [],
+                    quantity: ""
+                }]
+            },
+            profile: {
+                firstname: "",
+                lastname: "",
+                phone: "",
+                address: "",
+            }
+        }
+    }
 
-        //this.orders = this.props.history.location.query.orders;
-        //console.log(this.orders);
-        this.orders = {
-            orderId: "001",
-            timemillis: "08-02-2021",
-            totalprice: 20,
-            products: [{
-                name: "berryTea",
-                size: "small",
-                ice: "ice",
-                sweetness: "sweet",
-                extras: "ex",
-                quantity: 1
-            }]
-        };
-        this.location = "75 Service St, San Jose, CA 95112";
+    componentDidMount(){
+        const orderData = this.props.location.state.order;
+        const userData = this.props.user.userData;
+        console.log("Mount");
+        console.log(userData);
+        if(orderData && userData.profile){
+            this.setState({order: orderData});
+            this.setState({profile: userData.profile});
+        }
+        console.log(this.state);
     }
 
     render() {
+        console.log("render");
+        console.log(this.state);
+
+        const listProducts = this.state.order.products.map((products) =>
+            <div>
+                <p className="inlineP">{products.name}, </p>
+                <p className="geryP">{products.size} size, {products.ice}, {products.sweetness}, x{products.quantity}</p>
+            </div>
+        );
+
         return (
             <div className="container-confirm">
                 <div className="card" id="LocationConfirm">
                     <div className="card-body">
                         <h6>Pick-Up from</h6>
                         <h3>Street</h3>
-                        <h6>{this.location} </h6>
+                        <h6>{this.state.order.location} </h6>
                     </div>
                 </div>
 
                 <div className="card" id="PickUpConfirm">
                     <div className="card-body">
-                        <h4>Pick-Up time</h4>
+                        <h4>Pick-Up ASAP</h4>
                         <hr className="solid"></hr>
                         <h4>Contact Information</h4>
-                        <h6>name, phone</h6>
+                        <h6>{this.state.profile.firstname} {this.state.profile.lastname}, {this.state.profile.phone}</h6>
                     </div>
                 </div>
 
@@ -71,12 +100,12 @@ class ConfirmationPage extends React.Component {
                     <div className="card-body">
                         <h4>Your Order</h4>
                         <hr className="solid"></hr>
-                        <p>{this.orders.products[0].name}</p>
+                        <p>{listProducts}</p>
                         <hr className="solid"></hr>
                         <p>Subtotal</p>
                         <p>Discounts</p>
                         <h4 className="priceConfirm1">Total</h4>
-                        <h4 className="priceConfirm2">${this.orders.totalprice}</h4>
+                        <h4 className="priceConfirm2">${this.state.order.price}</h4>
                     </div>
                 </div>
 
@@ -84,5 +113,7 @@ class ConfirmationPage extends React.Component {
         );
     }
 }
-
-export default ConfirmationPage;
+export default connect(
+    state => ({user: state.fetchreducer}),
+    {fetchData}
+)(ConfirmationPage);
